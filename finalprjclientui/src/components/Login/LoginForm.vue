149909a6302480ea2ea73f149416ac787dba37d8
Logan Davis <davis0892@saskpolytech.ca>
2022-12-04 20:47:44 -0600

    Made Slight changes to how forms work. They are now back in the componants

--- finalprjclientui/src/components/Login/LoginForm.vue
+++ finalprjclientui/src/components/Login/LoginForm.vue
@@ -5,11 +5,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <b-form>
-      <b-form-group id="input-group-1" label="UserName" label-for="input-1">
-        <b-form-input id="input-1" placeholder="Enter Username" required></b-form-input>
+      <b-form-group id="UserName" label="UserName" label-for="Username-input">
+        <b-form-input id="Username-input" placeholder="Enter Username" required></b-form-input>
       </b-form-group>
-      <b-form-group id="input-group-2" label="PassWord" label-for="input-2">
-        <b-form-input id="input-2" placeholder="Enter Password" required></b-form-input>
+      <b-form-group id="PassWord" label="PassWord" label-for="Password-input">
+        <b-form-input id="Password-input" placeholder="Enter Password" required></b-form-input>
       </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
@@ -17,11 +17,12 @@
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Mixins, Vue } from 'vue-property-decorator';
 import {
   BButton,
   BForm, BFormGroup, BFormInput, BFormText,
 } from 'bootstrap-vue';
+import GlobalMixin from '@/mixins/global-mixin';
 
 Vue.component('BForm', BForm);
 Vue.component('BFormText', BFormText);
@@ -30,7 +31,8 @@ Vue.component('BFormGroup', BFormGroup);
 Vue.component('BButton', BButton);
 
 @Component
-export default class LoginForm extends Vue {}
+export default class LoginForm extends Mixins(GlobalMixin) {
+}
 </script>
 <style>
 .Flex{
